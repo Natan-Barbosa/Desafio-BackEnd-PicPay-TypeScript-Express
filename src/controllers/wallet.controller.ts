c71929fc552e4286acba2e4ff183d939bc581dcc
natan <natanbarbosax193@gmail.com>
2025-08-03 20:25:17 -0300

    feat: add increase balance endpoint

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { inject, injectable } from "tsyringe";
 import { WalletCreateDto } from "@/dto/wallet/wallet.create.dto";
 import { StatusCodes } from "http-status-codes";
+import { IncreaseBalanceDto } from "@/dto/wallet/wallet.increase.dto";
 
 @injectable()
 export class WalletController {
@@ -15,5 +16,11 @@ export class WalletController {
         const data: WalletCreateDto = req.body;
         const serviceResponse = await this.walletService.create(data);
         res.status(StatusCodes.CREATED).send(serviceResponse);
+    }
+
+    public async IncreaseBalance(req: Request, res: Response) {
+        const data: IncreaseBalanceDto = req.body
+        const serviceResponse = await this.walletService.IncreaseBalance(data)
+        return res.status(StatusCodes.ACCEPTED).send(serviceResponse)
     }
 }
