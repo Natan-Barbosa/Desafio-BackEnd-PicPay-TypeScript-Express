3491faf66586fd038892770863328b311ae31849
natan <natanbarbosax193@gmail.com>
2025-08-03 20:48:38 -0300

    fix: remove try catch because we have error middleware to log and handle error

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -2,10 +2,8 @@ import { WalletService } from "@/services/wallet.service";
 import { Request, Response } from "express";
 import { inject, injectable } from "tsyringe";
 import { WalletCreateDto } from "@/dto/wallet/wallet.create.dto";
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import { IncreaseBalanceDto } from "@/dto/wallet/wallet.increase.dto";
-import { logger } from "@/config/jet.logger.config";
-import { HttpException } from "@/utils/http.exception";
 
 @injectable()
 export class WalletController {
@@ -15,26 +13,14 @@ export class WalletController {
     ) { }
 
     public async create(req: Request, res: Response) {
-        try {
-            const data: WalletCreateDto = req.body;
-            const serviceResponse = await this.walletService.create(data);
-            res.status(StatusCodes.CREATED).send(serviceResponse);
-        } catch (error) {
-            logger.err(error)
-            throw new HttpException(ReasonPhrases.INTERNAL_SERVER_ERROR, StatusCodes.INTERNAL_SERVER_ERROR, "An Erro Ocorrend In Server")
-        }
-
+        const data: WalletCreateDto = req.body;
+        const serviceResponse = await this.walletService.create(data);
+        res.status(StatusCodes.CREATED).send(serviceResponse);
     }
 
     public async IncreaseBalance(req: Request, res: Response) {
-        try {
-            const data: IncreaseBalanceDto = req.body
-            const serviceResponse = await this.walletService.IncreaseBalance(data)
-            return res.status(StatusCodes.ACCEPTED).send(serviceResponse)
-        } catch (error) {
-            logger.err(error)
-            throw new HttpException(ReasonPhrases.INTERNAL_SERVER_ERROR, StatusCodes.INTERNAL_SERVER_ERROR, "An Erro Ocorrend In Server")
-        }
-
+        const data: IncreaseBalanceDto = req.body
+        const serviceResponse = await this.walletService.IncreaseBalance(data)
+        return res.status(StatusCodes.ACCEPTED).send(serviceResponse)
     }
 }
