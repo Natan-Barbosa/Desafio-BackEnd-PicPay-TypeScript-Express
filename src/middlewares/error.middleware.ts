a3559be32ac183fe1731fe0321c0926994012ccf
natan <natanbarbosax193@gmail.com>
2025-08-03 20:48:52 -0300

    feat: add logger to error

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,3 +1,4 @@
+import { logger } from "@/config/jet.logger.config";
 import { HttpException } from "@/utils/http.exception";
 import { NextFunction, Request, Response } from "express";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
@@ -6,5 +7,6 @@ export function ErrorMiddleware(err: Error, _req: Request, res: Response, _next:
     if (err instanceof HttpException) {
         return res.status(err.code).send(err)
     }
+    logger.err(err)
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ReasonPhrases.INTERNAL_SERVER_ERROR)
 }
