397dbba527fbefb8edba8e7ad79144937ab209ce
natan <natanbarbosax193@gmail.com>
2025-08-03 20:24:22 -0300

    feat: add increase balance method

--- src/services/wallet.service.ts
+++ src/services/wallet.service.ts
@@ -1,4 +1,5 @@
 import { WalletCreateDto } from "@/dto/wallet/wallet.create.dto";
+import { IncreaseBalanceDto } from "@/dto/wallet/wallet.increase.dto";
 import { WalletRepository } from "@/repository/wallet.repository";
 import { HttpException } from "@/utils/http.exception";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
@@ -17,5 +18,14 @@ export class WalletService {
       throw new HttpException(ReasonPhrases.BAD_REQUEST, StatusCodes.BAD_REQUEST, "Wallet Already Exists")
     }
     return await this.walletRepository.create(dto);
+  }
+
+  public async IncreaseBalance(dto: IncreaseBalanceDto) {
+    const wallet = await this.walletRepository.
+      findByCpfCnpj(dto.cpfOrCnpj)
+    if (!wallet) {
+      throw new HttpException(ReasonPhrases.BAD_REQUEST, StatusCodes.BAD_REQUEST, "Wallet Not Found!")
+    }
+    return await this.walletRepository.update(dto)
   }
 }
