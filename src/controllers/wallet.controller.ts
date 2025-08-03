02f73a8e9e0ba5433be5fe8a805797ca1ee75376
natan <natanbarbosax193@gmail.com>
2025-08-03 20:38:49 -0300

    fix: add try catch block to capture errors

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -2,8 +2,10 @@ import { WalletService } from "@/services/wallet.service";
 import { Request, Response } from "express";
 import { inject, injectable } from "tsyringe";
 import { WalletCreateDto } from "@/dto/wallet/wallet.create.dto";
-import { StatusCodes } from "http-status-codes";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { IncreaseBalanceDto } from "@/dto/wallet/wallet.increase.dto";
+import { logger } from "@/config/jet.logger.config";
+import { HttpException } from "@/utils/http.exception";
 
 @injectable()
 export class WalletController {
@@ -13,14 +15,26 @@ export class WalletController {
     ) { }
 
     public async create(req: Request, res: Response) {
-        const data: WalletCreateDto = req.body;
-        const serviceResponse = await this.walletService.create(data);
-        res.status(StatusCodes.CREATED).send(serviceResponse);
+        try {
+            const data: WalletCreateDto = req.body;
+            const serviceResponse = await this.walletService.create(data);
+            res.status(StatusCodes.CREATED).send(serviceResponse);
+        } catch (error) {
+            logger.err(error)
+            throw new HttpException(ReasonPhrases.INTERNAL_SERVER_ERROR, StatusCodes.INTERNAL_SERVER_ERROR, "An Erro Ocorrend In Server")
+        }
+
     }
 
     public async IncreaseBalance(req: Request, res: Response) {
-        const data: IncreaseBalanceDto = req.body
-        const serviceResponse = await this.walletService.IncreaseBalance(data)
-        return res.status(StatusCodes.ACCEPTED).send(serviceResponse)
+        try {
+            const data: IncreaseBalanceDto = req.body
+            const serviceResponse = await this.walletService.IncreaseBalance(data)
+            return res.status(StatusCodes.ACCEPTED).send(serviceResponse)
+        } catch (error) {
+            logger.err(error)
+            throw new HttpException(ReasonPhrases.INTERNAL_SERVER_ERROR, StatusCodes.INTERNAL_SERVER_ERROR, "An Erro Ocorrend In Server")
+        }
+
     }
 }
