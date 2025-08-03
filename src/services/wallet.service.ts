22ab7ebf917c9e251b896db1a4f1d1d0df8debc5
natan <natanbarbosax193@gmail.com>
2025-08-03 20:02:14 -0300

    feat: add validation to previne duplicated wallet

--- src/services/wallet.service.ts
+++ src/services/wallet.service.ts
@@ -1,14 +1,21 @@
 import { WalletCreateDto } from "@/dto/wallet/wallet.create.dto";
 import { WalletRepository } from "@/repository/wallet.repository";
+import { HttpException } from "@/utils/http.exception";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { inject, injectable } from "tsyringe";
 
 @injectable()
 export class WalletService {
   constructor(
     @inject("WalletRepository") private walletRepository: WalletRepository
-  ) {}
+  ) { }
 
   public async create(dto: WalletCreateDto) {
+    const wallet = await this.walletRepository.
+      findByEmailOrCpfCnpj(dto.email, dto.cpfOrCnpj)
+    if (wallet) {
+      throw new HttpException(ReasonPhrases.BAD_REQUEST, StatusCodes.BAD_REQUEST, "Wallet Already Exists")
+    }
     return await this.walletRepository.create(dto);
   }
 }
