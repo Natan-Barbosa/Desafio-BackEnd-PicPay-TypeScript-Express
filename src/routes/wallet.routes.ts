85417030942832bda4482bfbb543e991fbd871f9
natan <natanbarbosax193@gmail.com>
2025-08-03 20:39:11 -0300

    feature: add increase router

--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -2,6 +2,7 @@ import { WalletCreateDto } from "@/dto/wallet/wallet.create.dto";
 import { ValidationMiddleware } from "@/middlewares/validation.middleware";
 import express, { NextFunction, Request, Response } from "express";
 import { Controller as WalletController } from "@/containers/wallet.container";
+import { IncreaseBalanceDto } from "@/dto/wallet/wallet.increase.dto";
 
 const router = express.Router();
 
@@ -10,5 +11,10 @@ router.post(
   (req: Request, res: Response, next: NextFunction) => ValidationMiddleware(WalletCreateDto, req, res, next),
   (req: Request, res: Response) => WalletController.create(req, res)
 );
+
+router.post(
+  "/increase",
+  (req: Request, res: Response, next: NextFunction) => ValidationMiddleware(IncreaseBalanceDto, req, res, next),
+  (req: Request, res: Response) => WalletController.IncreaseBalance(req, res))
 
 export { router as WalletRoutes };
